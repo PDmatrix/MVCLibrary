5b6aa5c540317513733bab86a151e62aea0e07dc
PDmatrix <dimasebakovgoo@gmail.com>
2018-03-21 15:19:56 +0300

    v0.1 Первый коммит

---
+++ Pract/Models/LibContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Pract.Models
+{
+    public class LibContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Receipt> Receipts { get; set; }
+    }
+}
---
+++ Pract/Models/Receipt.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Pract.Models
+{
+    public class Receipt
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int BookId { get; set; }
+        //[DisplayFormat(DataFormatString = "{0:dd.MM.yyyy}", ApplyFormatInEditMode = true)]
+        //public DateTime Date { get; set; }
+        public User User { get; set; }
+        public Book Book { get; set; }
+    }
+}
